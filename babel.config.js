29f198a50a25733fbdaa0050f325fbdc755021de
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-14 07:51:51 -0300

    chore: configura alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-};
+	presets: ['module:@react-native/babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+				alias: {
+					'@infrastructure': './src/infrastructure',
+					'@data': './src/data',
+					'@domain': './src/domain',
+					'@components': './src/presentation/components',
+					'@modules': './src/presentation/modules',
+					'@images': './src/presentation/images',
+					'@presentation': './src/presentation',
+				},
+			},
+		],
+	],
+}
