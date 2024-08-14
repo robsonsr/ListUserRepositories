0eae0418332896581482d02b20e55ae766081ded
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-14 07:32:57 -0300

    chore: ajuste prettier e eslint

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
+  arrowParens: 'always',
+  bracketSameLine: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  semi: false,
+  tabWidth: 4,
+  useTabs: true,
 };
