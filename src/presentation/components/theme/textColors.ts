9772249197d91dc0bbc1ce9279b0c7007f0f0bef
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 16:13:32 -0300

    style: ajuste theme

---
+++ src/presentation/components/theme/textColors.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme, css } from 'styled-components/native'
+import { applyColor } from './utils'
+
+interface TextColors {
+	color?: keyof DefaultTheme['colors']
+}
+
+const textColors = css<TextColors>`
+	${({ color, theme }) => color && { color: applyColor(color, theme) }}
+`
+
+export { textColors }
+export type { TextColors }
