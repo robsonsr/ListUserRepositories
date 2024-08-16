dae2b20d8fad6faf3f5b604d59a80e1194f43b9d
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 16:25:55 -0300

    chore: componente UserNotFoundState

---
+++ src/presentation/screens/ListRepositoriesScreen/components/UserNotFoundState/UserNotFoundState.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Box, MessageState, Typograph } from '@components'
+
+interface UserNotFoundStateProps {
+	login: string
+}
+
+const UserNotFoundState = ({ login }: UserNotFoundStateProps) => {
+	return (
+		<Box justifyContent="center" alignItems="center" mt="sp5">
+			<Typograph mt="sp2" variant="heading4SemiBold" color="primary">
+				"{login}"
+			</Typograph>
+			<MessageState title="Nenhum usuário encontrado" description="Verifique se a escrita está correta ou tente novamente" />
+		</Box>
+	)
+}
+
+export { UserNotFoundState }
