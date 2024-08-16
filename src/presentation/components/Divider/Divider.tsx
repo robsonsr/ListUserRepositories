c5bf2778d1afdafe8dbffcb80b2f31f3c3351769
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 16:12:17 -0300

    chore: componente Divide

---
+++ src/presentation/components/Divider/Divider.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Box, BoxProps } from '@components'
+
+const Divider = (props: BoxProps) => {
+	return <Box height={1} bg="line" {...props} />
+}
+
+export { Divider }
