3de778c208eee28a8c0c7d8e8e548fefc8943f60
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 16:21:27 -0300

    chore: atom favorites

---
+++ src/presentation/atoms/favoriteRepositories/favoriteRepositories.ts
@@ -0,0 +1,38 @@
+import { Repository } from '@domain/models'
+import { storage } from '@infrastructure/storage/storage'
+import { atom, AtomEffect, DefaultValue } from 'recoil'
+
+type FavoriteRepositoriesState = Repository[]
+
+const persistAtom: AtomEffect<any> = ({ node, setSelf, onSet }) => {
+	const loadPersisted = async () => {
+		try {
+			const savedValue = await storage.getItem(node.key)
+			if (savedValue != null) {
+				setSelf(savedValue)
+			}
+		} catch (error) {
+			console.error(`Error loading ${node.key} from AsyncStorage`, error)
+			setSelf(new DefaultValue())
+		}
+	}
+
+	loadPersisted()
+
+	onSet((newValue) => {
+		if (newValue instanceof DefaultValue) {
+			storage.removeItem(node.key)
+		} else {
+			storage.setItem(node.key, newValue)
+		}
+	})
+}
+
+const favoriteRepositoriesState = atom<FavoriteRepositoriesState>({
+	key: 'favoriteRepositoriesState',
+	default: [],
+	effects: [persistAtom],
+})
+
+export { favoriteRepositoriesState }
+export type { FavoriteRepositoriesState }
