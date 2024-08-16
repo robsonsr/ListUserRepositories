26146cd6c8ca32257cab8277295955b2ed656ed3
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 16:19:21 -0300

    chore: ajuste Typograph

--- src/presentation/components/Typograph/Typograph.tsx
+++ src/presentation/components/Typograph/Typograph.tsx
@@ -4,14 +4,6 @@ import styled from 'styled-components/native'
 
 import * as T from '../theme'
 
-const FONT_FAMILIES = {
-	Poppins: {
-		regular: 'Poppins-Regular',
-		medium: 'Poppins-Medium',
-		SemiBold: 'Poppins-SemiBold',
-	},
-}
-
 type Variants =
 	| 'heading1SemiBold'
 	| 'heading2SemiBold'
@@ -23,51 +15,52 @@ type Variants =
 	| 'paragraphySmRegular'
 	| 'paragraphySmMedium'
 
-interface TypographProps extends TextProps, T.Space, T.TextColors {
+interface TypographProps extends TextProps, T.Space, T.TextColors, T.Text {
 	variant?: Variants
 }
 
 const Typograph = styled.Text<TypographProps>`
 	${T.space}
 	${T.textColors}
+	${T.text}
     ${({ variant, color, theme }) => {
 		const paragraphyMdRegular = {
-			fontFamily: FONT_FAMILIES.Poppins.regular,
+			fontFamily: theme.fonts.weight.regular,
 			fontSize: 14,
 			color: color ? theme.colors[color] : theme.colors.darkNeutral,
 		}
 
 		if (variant === 'heading1SemiBold')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.SemiBold,
+				fontFamily: theme.fonts.weight.SemiBold,
 				fontSize: 21,
 				color: color ? theme.colors[color] : theme.colors.neutral,
 			}
 
 		if (variant === 'heading2SemiBold')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.SemiBold,
+				fontFamily: theme.fonts.weight.SemiBold,
 				fontSize: 18,
 				color: color ? theme.colors[color] : theme.colors.neutral,
 			}
 
 		if (variant === 'heading3Medium')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.medium,
+				fontFamily: theme.fonts.weight.medium,
 				fontSize: 18,
 				color: color ? theme.colors[color] : theme.colors.neutral,
 			}
 
 		if (variant === 'heading4SemiBold')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.SemiBold,
+				fontFamily: theme.fonts.weight.SemiBold,
 				fontSize: 16,
 				color: color ? theme.colors[color] : theme.colors.neutral,
 			}
 
 		if (variant === 'heading5Regular')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.regular,
+				fontFamily: theme.fonts.weight.regular,
 				fontSize: 16,
 				color: color ? theme.colors[color] : theme.colors.neutral,
 			}
@@ -76,21 +69,21 @@ const Typograph = styled.Text<TypographProps>`
 
 		if (variant === 'paragraphyMdMedium')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.medium,
+				fontFamily: theme.fonts.weight.medium,
 				fontSize: 14,
 				color: color ? theme.colors[color] : theme.colors.darkNeutral,
 			}
 
 		if (variant === 'paragraphySmRegular')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.regular,
+				fontFamily: theme.fonts.weight.regular,
 				fontSize: 12,
 				color: color ? theme.colors[color] : theme.colors.placeholder,
 			}
 
 		if (variant === 'paragraphySmMedium')
 			return {
-				fontFamily: FONT_FAMILIES.Poppins.medium,
+				fontFamily: theme.fonts.weight.medium,
 				fontSize: 12,
 				color: color ? theme.colors[color] : theme.colors.darkNeutral,
 			}
