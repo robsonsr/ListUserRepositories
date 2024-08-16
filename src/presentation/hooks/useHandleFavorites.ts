95e1052393555a7346b4af15793c8f9d20a2e8ee
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 16:21:50 -0300

    chore: hook useHandleFavorites

---
+++ src/presentation/hooks/useHandleFavorites.ts
@@ -0,0 +1,26 @@
+import { Repository } from '@domain/models'
+import { favoriteRepositoriesState } from '@presentation/atoms/favoriteRepositories'
+import { useCallback } from 'react'
+import { useRecoilState } from 'recoil'
+
+const useHandleFavorites = () => {
+	const [favorites, setFavorites] = useRecoilState(favoriteRepositoriesState)
+
+	const handleFavorites = useCallback(
+		(repository: Repository) => {
+			setFavorites((prev) => {
+				const alreadyFavorited = prev.some((item) => item.id === repository.id)
+				if (alreadyFavorited) return [...prev.filter((item) => item.id !== repository.id)]
+				return [...prev, repository]
+			})
+		},
+		[setFavorites],
+	)
+
+	return {
+		favorites,
+		handleFavorites,
+	}
+}
+
+export { useHandleFavorites }
