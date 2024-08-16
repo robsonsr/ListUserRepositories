3a125a55a3ca81133791bad2a1bcca0f8e45f4c8
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 16:15:18 -0300

    chore: componente RepositoryCard

---
+++ src/presentation/components/RepositoryCard/RepositoryCard.tsx
@@ -0,0 +1,61 @@
+import React, { memo } from 'react'
+import { Card } from '@components/Card'
+import { Box, Icon, Row, Typograph } from '@components'
+import { Repository } from '@domain/models'
+import { Patterns, dateUtil } from '@infrastructure/dateUtil'
+
+interface RepositoryCardProps {
+	respository: Repository
+	isFavorite: boolean
+	onPress: (repository: Repository) => void
+}
+
+const RepositoryCard = memo(({ respository, isFavorite, onPress }: RepositoryCardProps) => {
+	return (
+		<Card
+			mt="sp4"
+			onPress={() => {
+				onPress(respository)
+			}}
+		>
+			<Row justifyContent="space-between">
+				<Box flex={1}>
+					<Typograph numberOfLines={1} variant="heading2SemiBold">
+						{respository.name}
+					</Typograph>
+				</Box>
+
+				<Box
+					borderWidth={1}
+					radius="full"
+					height={32}
+					width={32}
+					alignItems="center"
+					justifyContent="center"
+					borderColor={isFavorite ? 'primary' : 'backgroundSecondary'}
+					bg={isFavorite ? 'background' : 'backgroundSecondary'}
+				>
+					<Icon name="heart" size="small" color={isFavorite ? 'iconActive' : 'iconNeutral'} />
+				</Box>
+			</Row>
+
+			<Typograph variant="paragraphyMdRegular" mt="sp2">
+				{respository?.description ? respository.description : 'N/A'}
+			</Typograph>
+
+			<Row alignItems="center">
+				{respository.primaryLanguage?.name && (
+					<>
+						<Box height={16} width={16} bg={respository.primaryLanguage?.color} radius="full" />
+						<Typograph variant="paragraphyMdRegular" mt="sp2" ml="sp2">
+							{respository.primaryLanguage?.name}
+						</Typograph>
+					</>
+				)}
+			</Row>
+			{respository?.updatedAt && <Typograph>Updated on {dateUtil.format(respository.updatedAt, Patterns.dMMMyyyy)}</Typograph>}
+		</Card>
+	)
+})
+
+export { RepositoryCard }
