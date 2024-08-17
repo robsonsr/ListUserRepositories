ecbacd1618c7feaa16084820f032381ef69d6a50
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-16 21:53:04 -0300

    configura ambiente de test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  preset: 'react-native',
-};
+	preset: 'react-native',
+	setupFiles: ['./jest.setup.js'],
+	transformIgnorePatterns: ['node_modules/(?!(@react-native|react-native|@react-navigation|recoil)/)'],
+	moduleNameMapper: {
+		recoil: 'recoil/native',
+	},
+}
