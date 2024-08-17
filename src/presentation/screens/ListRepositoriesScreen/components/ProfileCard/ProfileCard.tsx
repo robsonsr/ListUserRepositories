9d0e717c46b445f9efa55bd4e58af51c2a2e80f4
Robson Rodrigues <robsonrodrigues@Robsons-MacBook-Air.local>
2024-08-17 08:22:10 -0300

    style: adiciona divider ProfileCard

--- src/presentation/screens/ListRepositoriesScreen/components/ProfileCard/ProfileCard.tsx
+++ src/presentation/screens/ListRepositoriesScreen/components/ProfileCard/ProfileCard.tsx
@@ -3,6 +3,7 @@ import { Image } from 'react-native'
 
 import { Box, Column, Row, Typograph } from '@components'
 import { Card } from '@components/Card'
+import { Divider } from '@components/Divider'
 import { Profile } from '@domain/models'
 
 interface ProfileCardProps {
@@ -13,7 +14,14 @@ const ProfileCard = memo(({ profile }: ProfileCardProps) => {
 	return (
 		<Card mt="sp4">
 			<Row>
-				<Box bg="backgroundSecondary" radius="full" height={48} width={48} justifyContent="center" alignItems="center">
+				<Box
+					bg="backgroundSecondary"
+					radius="full"
+					height={48}
+					width={48}
+					justifyContent="center"
+					alignItems="center"
+				>
 					<Image source={{ uri: profile.avatarUrl }} height={48} width={48} borderRadius={48} />
 				</Box>
 
@@ -22,6 +30,8 @@ const ProfileCard = memo(({ profile }: ProfileCardProps) => {
 					<Typograph variant="paragraphyMdRegular">@{profile.login}</Typograph>
 				</Column>
 			</Row>
+
+			<Divider mv="sp2" />
 
 			<Typograph variant="paragraphyMdRegular" mt="sp2">
 				{profile.bio ? profile.bio : 'Sem descrição'}
